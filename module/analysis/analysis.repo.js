c6cf43c03e15c9ce2ebb6a27bf03b22e0a63ebce
Damitha Perera <damithaperera777@gmail.com>
2023-03-13 22:46:04 +0530

    change

--- module/analysis/analysis.repo.js
+++ module/analysis/analysis.repo.js
@@ -9,8 +9,8 @@ exports.analysisRepo = async (name, description, owner) => {
     return playListModel.find({
         $or: [
             {name: {$regex: name, $options: 'i'}},
-            {description: {$regex: description, $options: 'i'}},
-            {'owner.name': {$regex: owner, $options: 'i'}},
+            // {description: {$regex: description, $options: 'i'}},
+            // {'owner.name': {$regex: owner, $options: 'i'}},
         ]
     }).sort({createdAt: -1})
 }
